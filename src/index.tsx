f25f75b561312254547d62d1351d5af968280af6
Manuel Selch <manuel@selch.de>
2025-07-12 23:03:18 +0200

    setup react

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import '@mantine/core/styles.css';
+import { MantineProvider, Container, Grid, Text, Button } from '@mantine/core';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
-    <App />
+    <MantineProvider>
+      <Container>
+        <Text>Hello World</Text>
+      </Container>
+    </MantineProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
